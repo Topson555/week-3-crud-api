87488646e45e6b9ea0f3d2bf7ba503c2cb2628eb
Okewale Emmanuel Tope <okewaleemmanuel211@gmailcom>
2026-03-31 15:01:16 -0700

    chore: add CORS middleware to enable cross-origin requests

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-app.use(express.json());  
+app.use(express.json());
+const cors = require('cors');
+app.use(cors());  
 
 let todos = [
   { id: 1, task: 'Learn Node.js', completed: false },
